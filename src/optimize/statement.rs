45787a28d555fe6120d6c34789e710da48a4f666
Johan-Mi <johanmi@protonmail.com>
2022-07-25 16:29:19 +0200

    More optimizations

--- src/optimize/statement.rs
+++ src/optimize/statement.rs
@@ -1,8 +1,11 @@
 use crate::{
-    ir::proc::Statement::{self, *},
+    ir::{
+        expr::Expr::Lit,
+        proc::Statement::{self, *},
+    },
     optimize::expr::optimize_expr,
 };
-use trexp::{Bind, Clean, Rewrite, TreeWalk};
+use trexp::{Bind, Clean, Dirty, Rewrite, TreeWalk};
 
 pub fn optimize_stmt(stmt: Statement) -> Rewrite<Statement> {
     Rewrite::repeat(stmt, |s| {
@@ -13,8 +16,9 @@ pub fn optimize_stmt(stmt: Statement) -> Rewrite<Statement> {
 }
 
 const STMT_OPTIMIZATIONS: &[fn(Statement) -> Rewrite<Statement>] =
-    &[optimize_stmt_exprs];
+    &[optimize_stmt_exprs, flatten_do, const_conditions];
 
+/// Optimizes all expressions contained in a statement.
 fn optimize_stmt_exprs(stmt: Statement) -> Rewrite<Statement> {
     match stmt {
         Do(_) | Forever(_) => Clean(stmt),
@@ -50,5 +54,54 @@ fn optimize_stmt_exprs(stmt: Statement) -> Rewrite<Statement> {
             times,
             body,
         }),
+    }
+}
+
+/// Flattens nested `do` blocks.
+fn flatten_do(stmt: Statement) -> Rewrite<Statement> {
+    match stmt {
+        Do(mut stmts) if stmts.len() == 1 => Dirty(stmts.pop().unwrap()),
+        Do(stmts) if stmts.iter().any(|stmt| matches!(stmt, Do(_))) => {
+            Dirty(Do(stmts
+                .into_iter()
+                .flat_map(|stmt| match stmt {
+                    Do(nested) => nested,
+                    _ => vec![stmt],
+                })
+                .collect()))
+        }
+        _ => Clean(stmt),
+    }
+}
+
+/// Removes constant conditions from if statements and loops.
+fn const_conditions(stmt: Statement) -> Rewrite<Statement> {
+    match stmt {
+        IfElse {
+            condition: Lit(condition),
+            if_true,
+            if_false,
+        } => Dirty(if condition.to_bool() {
+            *if_true
+        } else {
+            *if_false
+        }),
+        Until {
+            condition: Lit(condition),
+            body,
+        } => Dirty(if condition.to_bool() {
+            Do(Vec::new())
+        } else {
+            Forever(body)
+        }),
+        While {
+            condition: Lit(condition),
+            body,
+        } => Dirty(if condition.to_bool() {
+            Forever(body)
+        } else {
+            Do(Vec::new())
+        }),
+        _ => Clean(stmt),
     }
 }
