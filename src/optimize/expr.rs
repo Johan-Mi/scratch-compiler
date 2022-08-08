7104b886dfe43bda8de7292cba044b2d4ae93818
Johan-Mi <johanmi@protonmail.com>
2022-08-08 18:18:02 +0200

    More optimization rules

--- src/optimize/expr.rs
+++ src/optimize/expr.rs
@@ -18,7 +18,11 @@ const EXPR_OPTIMIZATIONS: &[fn(Expr) -> Rewrite<Expr>] = &[
     mul_identities,
     add_identity,
     trigonometry,
+    zero_minus,
     double_minus,
+    times_negation,
+    add_subtraction,
+    add_negation,
     redundant_to_num,
 ];
 
@@ -108,6 +112,18 @@ fn trigonometry(mut expr: Expr) -> Rewrite<Expr> {
     }
 }
 
+/// Subtracting something from zero is equivalent to negation.
+fn zero_minus(mut expr: Expr) -> Rewrite<Expr> {
+    if let FuncCall("-", _, args) = &mut expr
+      && matches!(&args[..], [Lit(Value::Num(num)), _] if *num == 0.0)
+    {
+        args.swap_remove(0);
+        Dirty(expr)
+    } else {
+        Clean(expr)
+    }
+}
+
 /// Double negation just converts the argument to a number.
 fn double_minus(mut expr: Expr) -> Rewrite<Expr> {
     if let FuncCall(outer_sym @ "-", _, outer_args) = &mut expr
@@ -117,6 +133,72 @@ fn double_minus(mut expr: Expr) -> Rewrite<Expr> {
         *outer_sym = "to-num";
         *outer_args = mem::take(args);
         Dirty(expr)
+    } else {
+        Clean(expr)
+    }
+}
+
+/// Floats negation in a multiplication outward.
+fn times_negation(mut expr: Expr) -> Rewrite<Expr> {
+    if let FuncCall("*", span, args) = &mut expr
+      && args.iter_mut().any(|e|
+        if let FuncCall("-", _, args) = e && args.len() == 1 {
+            *e = args.pop().unwrap();
+            true
+        } else {
+            false
+        }
+    )
+    {
+        Dirty(FuncCall("-", *span, vec![expr]))
+    } else {
+        Clean(expr)
+    }
+}
+
+/// Floats subtraction in addition outward.
+fn add_subtraction(mut expr: Expr) -> Rewrite<Expr> {
+    let mut subtracted = Vec::new();
+    if let FuncCall("+", span, ref mut terms) = expr {
+        for term in terms {
+            if let FuncCall("-", _, args) = term
+              && args.len() >= 2 {
+                let positive = args.swap_remove(0);
+                subtracted.append(args);
+                *term = positive;
+            }
+        }
+        if subtracted.is_empty() {
+            Clean(expr)
+        } else {
+            subtracted.insert(0, expr);
+            Dirty(FuncCall("-", span, subtracted))
+        }
+    } else {
+        Clean(expr)
+    }
+}
+
+/// Floats negation in addition outward.
+fn add_negation(mut expr: Expr) -> Rewrite<Expr> {
+    let mut subtracted = Vec::new();
+    if let FuncCall("+", span, ref mut terms) = expr {
+        let mut i = 0;
+        while i < terms.len() {
+            if let FuncCall("-", _, args) = &mut terms[i]
+              && args.len() == 1 {
+                subtracted.push(args.pop().unwrap());
+                terms.swap_remove(i);
+            } else {
+                i += 1;
+            }
+        }
+        if subtracted.is_empty() {
+            Clean(expr)
+        } else {
+            subtracted.insert(0, expr);
+            Dirty(FuncCall("-", span, subtracted))
+        }
     } else {
         Clean(expr)
     }
