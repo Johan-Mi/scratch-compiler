349afe2a952ef35a6d118b0e5f34e0cf579c1fdd
Johan-Mi <johanmi@protonmail.com>
2022-08-26 16:36:04 +0200

    Add lint for inconsistent node indentation

--- src/error.rs
+++ src/error.rs
@@ -262,7 +262,15 @@ impl Error {
 }
 
 pub enum Warning {
-    ParenTooFarLeft { left: Span, right: Span },
+    ParenTooFarLeft {
+        left: Span,
+        right: Span,
+    },
+    InconsistentIndentation {
+        node: Span,
+        good: Span,
+        offender: Span,
+    },
 }
 
 impl Warning {
@@ -274,6 +282,26 @@ impl Warning {
                 .with_labels(vec![primary(*right).with_message(
                     "this parenthesis is further to the left than its match",
                 ), secondary(*left).with_message("match is here")])],
+            InconsistentIndentation {
+                node,
+                good,
+                offender,
+            } => {
+                vec![Diagnostic::warning()
+                    .with_message("inconsistent indentation")
+                    .with_labels(vec![
+                    primary(*node).with_message(
+                        "nodes spanning multiple lines should have the same \
+                        level of indentation for all non-initial lines"
+                    ),
+                    secondary(*good).with_message(
+                        "if this item is indented correctly..."
+                    ),
+                    secondary(*offender).with_message(
+                        "...then this is not"
+                    ),
+                ])]
+            }
         };
 
         let writer = StandardStream::stderr(ColorChoice::Always);
--- src/lint.rs
+++ src/lint.rs
@@ -1,10 +1,11 @@
 use crate::{ast::Ast, error::Warning, span::Span, FILES};
-use codespan::ByteOffset;
+use codespan::{ByteOffset, Location};
 
 pub fn lint_ast(ast: &Ast) {
     match ast {
         Ast::Node(head, tail, span) => {
             paren_too_far_left(*span);
+            inconsistent_indentation(tail, *span);
             lint_ast(head);
             for ast in tail {
                 lint_ast(ast);
@@ -38,5 +39,42 @@ fn paren_too_far_left(span: Span) {
             },
         }
         .emit();
+    }
+}
+
+fn inconsistent_indentation(tail: &[Ast], span: Span) {
+    let files = FILES.lock().unwrap();
+    let mut current_line = files
+        .location(span.file, span.position.start())
+        .unwrap()
+        .line;
+    let mut prev_column = None;
+    let mut good = None;
+    for ast in tail {
+        let subspan = ast.span();
+        let Location { line, column } = files
+            .location(subspan.file, subspan.position.start())
+            .unwrap();
+        if line != current_line {
+            current_line = line;
+            match prev_column {
+                Some(prev_column) => {
+                    if column != prev_column {
+                        drop(files);
+                        Warning::InconsistentIndentation {
+                            node: span,
+                            good: good.unwrap(),
+                            offender: ast.span(),
+                        }
+                        .emit();
+                        return;
+                    }
+                }
+                None => {
+                    prev_column = Some(column);
+                    good = Some(ast.span());
+                }
+            }
+        }
     }
 }
