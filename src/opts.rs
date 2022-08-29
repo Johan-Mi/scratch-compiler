bdc0c1e4f4f8c2ff1a1954b2821721abee1ec281
Johan-Mi <johanmi@protonmail.com>
2022-08-29 15:38:50 +0200

    Make CLI flag documentation imperative

--- src/opts.rs
+++ src/opts.rs
@@ -4,30 +4,30 @@ use std::path::PathBuf;
 #[derive(Options)]
 /// Compiles Lisp code into Scratch projects.
 pub struct Opts {
-    /// Displays this help message
+    /// Display this help message
     pub help: bool,
 
     /// The source file to compile
     #[options(free, required)]
     pub file: PathBuf,
 
-    /// Runs the linter while compiling
+    /// Run the linter while compiling
     #[options(no_short)]
     pub lint: bool,
 
-    /// Dumps the initial AST to a file
+    /// Dump the initial AST to a file
     #[options(no_short, meta = "FILE")]
     pub dump_ast: Option<PathBuf>,
 
-    /// Dumps the expanded AST to a file
+    /// Dump the expanded AST to a file
     #[options(no_short, meta = "FILE")]
     pub dump_expanded: Option<PathBuf>,
 
-    /// Dumps the unoptimized IR to a file
+    /// Dump the unoptimized IR to a file
     #[options(no_short, meta = "FILE")]
     pub dump_ir: Option<PathBuf>,
 
-    /// Dumps the optimized IR to a file
+    /// Dump the optimized IR to a file
     #[options(no_short, meta = "FILE")]
     pub dump_optimized: Option<PathBuf>,
 }
