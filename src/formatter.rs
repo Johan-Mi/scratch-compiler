8fdd13b5315e47bbe843f9bf1c38b54f1dd774b5
Johan-Mi <johanmi@protonmail.com>
2024-02-13 18:15:53 +0100

    Just wrap `lispfmt::Token`

--- src/formatter.rs
+++ src/formatter.rs
@@ -5,13 +5,8 @@ use std::fmt;
 pub fn format_stdin_to_stdout() -> Result<(), std::io::Error> {
     let source_code = std::io::read_to_string(std::io::stdin())?;
     let mut tokens =
-        TokenKind::lexer(&source_code).map(|kind| match kind.unwrap() {
-            TokenKind::Atom(atom) => Token::Atom(atom),
-            TokenKind::Unquote => Token::PrefixOperator(","),
-            TokenKind::LParen => Token::LParen,
-            TokenKind::RParen => Token::RParen,
-            TokenKind::Comment(comment) => Token::Comment(comment),
-            TokenKind::NewLine => Token::NewLine,
+        WrappedToken::lexer(&source_code).map(|token| match token.unwrap() {
+            WrappedToken::Of(inner) => inner,
         });
     let output = lispfmt::format(&mut tokens, 2).unwrap();
     print!("{output}");
@@ -51,18 +46,13 @@ impl lispfmt::Atom for Atom<'_> {
 
 #[derive(Logos)]
 #[logos(skip r"[ \t]+")]
-enum TokenKind<'src> {
-    #[regex(r#"[^ \t\n,();"]+"#, |lex| Atom(lex.slice()))]
-    #[regex(r#""([^"\\\n]|\\.)*["\\]?"#, |lex| Atom(lex.slice()))]
-    Atom(Atom<'src>),
-    #[token(",")]
-    Unquote,
-    #[token("(")]
-    LParen,
-    #[token(")")]
-    RParen,
-    #[regex(r";.*\n?", |lex| lex.slice())]
-    Comment(&'src str),
-    #[token("\n")]
-    NewLine,
+enum WrappedToken<'src> {
+    #[regex(r#"[^ \t\n,();"]+"#, |lex| Token::Atom(Atom(lex.slice())))]
+    #[regex(r#""([^"\\\n]|\\.)*["\\]?"#, |lex| Token::Atom(Atom(lex.slice())))]
+    #[token(",", |_| Token::PrefixOperator(","))]
+    #[token("(", |_| Token::LParen)]
+    #[token(")", |_| Token::RParen)]
+    #[regex(r";.*\n?", |lex| Token::Comment(lex.slice()))]
+    #[token("\n", |_| Token::NewLine)]
+    Of(Token<'src, Atom<'src>>),
 }
