a4ff8e18c87cf9580248871570bdb2bf17c7cb7f
Johan-Mi <johanmi@protonmail.com>
2024-02-13 18:08:51 +0100

    Use Logos itself to attach source code to tokens
    This is shorter, simpler and more idiomatic.

--- src/formatter.rs
+++ src/formatter.rs
@@ -5,18 +5,14 @@ use std::fmt;
 pub fn format_stdin_to_stdout() -> Result<(), std::io::Error> {
     let source_code = std::io::read_to_string(std::io::stdin())?;
     let mut tokens =
-        TokenKind::lexer(&source_code)
-            .spanned()
-            .map(|(kind, span)| match kind.unwrap() {
-                TokenKind::Atom => Token::Atom(Atom(&source_code[span])),
-                TokenKind::Unquote => Token::PrefixOperator(&source_code[span]),
-                TokenKind::LParen => Token::LParen,
-                TokenKind::RParen => Token::RParen,
-                TokenKind::Comment => {
-                    Token::Comment(source_code[span].trim_end_matches('\n'))
-                }
-                TokenKind::NewLine => Token::NewLine,
-            });
+        TokenKind::lexer(&source_code).map(|kind| match kind.unwrap() {
+            TokenKind::Atom(atom) => Token::Atom(atom),
+            TokenKind::Unquote => Token::PrefixOperator(","),
+            TokenKind::LParen => Token::LParen,
+            TokenKind::RParen => Token::RParen,
+            TokenKind::Comment(comment) => Token::Comment(comment),
+            TokenKind::NewLine => Token::NewLine,
+        });
     let output = lispfmt::format(&mut tokens, 2).unwrap();
     print!("{output}");
     Ok(())
@@ -55,18 +51,18 @@ impl lispfmt::Atom for Atom<'_> {
 
 #[derive(Logos)]
 #[logos(skip r"[ \t]+")]
-enum TokenKind {
-    #[regex(r#"[^ \t\n,();"]+"#)]
-    #[regex(r#""([^"\\\n]|\\.)*["\\]?"#)]
-    Atom,
+enum TokenKind<'src> {
+    #[regex(r#"[^ \t\n,();"]+"#, |lex| Atom(lex.slice()))]
+    #[regex(r#""([^"\\\n]|\\.)*["\\]?"#, |lex| Atom(lex.slice()))]
+    Atom(Atom<'src>),
     #[token(",")]
     Unquote,
     #[token("(")]
     LParen,
     #[token(")")]
     RParen,
-    #[regex(r";.*\n?")]
-    Comment,
+    #[regex(r";.*\n?", |lex| lex.slice())]
+    Comment(&'src str),
     #[token("\n")]
     NewLine,
 }
